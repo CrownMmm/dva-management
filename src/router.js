b794a1789e6e29a0d1d0a61e017473d2bf03f062
zhangjiaming <823600537@qq.com>
2021-03-29 20:59:24 +0800

    feat:初始化项目

--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import IndexPage from './pages/IndexPage';
 
 function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={IndexPage} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route path="/" exact component={IndexPage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default RouterConfig;
