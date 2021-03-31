5b278d3c101bdfcc6ec8d3aa01c666dc83e9bde9
zhangjiaming <823600537@qq.com>
2021-03-31 22:20:26 +0800

    feat:添加navbar组件

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Layout } from 'antd';
+import NavBar from './Navbar';
+
 import styles from './IndexPage.scss';
 
 const { Header, Content } = Layout;
@@ -9,7 +11,9 @@ const { Header, Content } = Layout;
 function IndexPage() {
     return (
         <Layout className={styles.layout}>
-            <Header className={styles.header}>header</Header>
+            <Header className={styles.header}>
+                <NavBar />
+            </Header>
             <Content className={styles.content}>main content</Content>
         </Layout>
     );
